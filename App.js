719cf3c5d2d1ff41ab0a4a673b4443a060ff0ad6
szaszfaijulia <szaszfai.julia@gmail.com>
2022-04-09 14:22:23 +0200

    4 files modified, 1 added, 1 deleted - navbar

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./style.css";
-
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
     <div>
-      
+      <Navbar />
     </div>
   );
 }
---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+  return(
+    <nav>
+      <img src="https://raw.githubusercontent.com/szaszfaijulia/react-travelJournal/master/src/earth.png" className="nav--logo"/>
+      <h1 className="nav--title">my travel journal.</h1>
+    </nav>
+  )
+}
